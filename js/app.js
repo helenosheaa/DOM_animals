6bd249742d6a2d976b18e2d8ac91fff03c486ba6
helenosheaa <helenosheaa@gmail.com>
2018-08-03 11:13:19 +0100

    displays animals entered

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  const form = document.querySelector('#new-item-form');
 
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const species = event.target.species.value;
+    const continent = event.target.continent.value;
+
+    const formInput = document.querySelector('#animal-list');
+
+    const animalItem = document.createElement('p')
+
+    animalItem.textContent = `Name: ${name}, Species: ${species}, Continent: ${continent}`;
+
+    animalItem.classList.add("animal-item");
+
+    formInput.appendChild(animalItem);
+
+    form.reset();
+  });
 
 
 })
